346cd50afeb10964f5f9dc8ba22a33552761d55c
AprioriFox <apriorifox@gmail.com>
2021-11-12 19:47:34 +0600

    make some changes

--- StrLen.js
+++ StrLen.js
@@ -1,6 +1,7 @@
+let str = prompt("Введите строку");
+
 const countChar = (str) => {
     console.log("Количество символов в введенной строке " + str.length + " элементов");
 }
-let str = prompt("Введите строку");
 
 countChar(str);
